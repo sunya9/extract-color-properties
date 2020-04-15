da2bac6a1afcb6ba71518f7f49b34f1225546235
sunya9 <mugen.xyz@gmail.com>
2020-04-15 11:38:13 +0900

    test: add excludeProperties test

--- __tests__/extract-color-properties.spec.ts
+++ __tests__/extract-color-properties.spec.ts
@@ -1,6 +1,10 @@
 import * as fs from 'fs'
 import * as postcss from 'postcss'
-import { plugin, extractColorProperties } from '../lib/extract-color-properties'
+import {
+  plugin,
+  extractColorProperties,
+  Options,
+} from '../lib/extract-color-properties'
 import * as path from 'path'
 
 const inputCSSPath = path.resolve(__dirname, './fixtures/input.css')
@@ -10,12 +14,15 @@ const inputCSS = fs.readFileSync(inputCSSPath, { encoding: 'utf-8' })
 const outputCSS = fs.readFileSync(outputCSSPath, { encoding: 'utf-8' })
 
 describe('extract-color-properties', () => {
+  const options: Options = { excludeProperties: ['height'] }
   test('as plugin', () => {
-    const { css } = postcss([plugin()]).process(inputCSS, { from: undefined })
+    const { css } = postcss([plugin(options)]).process(inputCSS, {
+      from: undefined,
+    })
     expect(css).toEqual(outputCSS)
   })
   test('as method', () => {
-    const { css } = extractColorProperties(inputCSS)
+    const { css } = extractColorProperties(inputCSS, options)
     expect(css).toEqual(outputCSS)
   })
 })
